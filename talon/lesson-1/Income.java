686298aa01201af1ac3cb847dbd599834117deb5
Gabriel Santos <gabr1el_santos@hotmail.com>
2020-02-06 21:23:13 -0300

    Created the intro exercises

---
+++ talon/lesson-1/Income.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Income {
+  public static void main(String[] args){
+    Scanner read = new Scanner(System.in);
+
+    System.out.println("Please, insert your old sallary: ");
+    double income = read.nextDouble();
+
+    System.out.println("Your new sallary is: " + income * 1.25);
+  }
+}
